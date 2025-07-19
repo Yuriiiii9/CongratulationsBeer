65787d9dda4ba241e2a24d728447f54c5af0b282
Yuri Xu <yurioxu289@gmail.com>
2025-07-18 20:20:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -227,8 +227,8 @@ with tab1:
     powerbi_iframe = """
     <iframe
         title="Nonny Beer Dashboard_new"
-        width="1200" 
-        height="541.25"
+        width="100%" 
+        height="1000"
         src="https://app.powerbi.com/reportEmbed?reportId=bb9fc62e-298e-4d1f-8bad-4d11c1570431&autoAuth=true&ctid=92315d43-67d5-4613-9f3d-c3fb5114bf50"
         frameborder="0"
         allowFullScreen="true">
